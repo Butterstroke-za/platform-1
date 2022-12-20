3a2540a23c7ae300e8c13e6e2ac5ea4cb4e8d455
Koketso <koketsomawasha@gmail.com>
2022-12-20 12:31:37 +0200

    Server connection complete

--- app.js
+++ app.js
@@ -5,18 +5,8 @@ const app = express()
 
 // TODO:
 // create app routes
-// create models (own folder)
-// create controllers (own folder)
-// create routes (own folder)
-// link to database 
-
-
-
-const port = 3000;
-app.listen(port, () =>{
-    console.log('connection succeeded....')
-})
-
+//middleware
+// mount routes
 
 
 module.exports = app
