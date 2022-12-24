2597843d8f66f5843aa8d05bdf87ec0521169db7
Koketso <koketsomawasha@gmail.com>
2022-12-24 21:42:30 +0200

    user routes and controllers defined

--- models/User.js
+++ models/User.js
@@ -1,25 +1,17 @@
 const mongoose = require('mongoose')
-const { response } = require('../app')
 const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
     fullName: {
         type: String, 
         required: [true, 'Your full name is required.'], 
-        trim: true, 
-        validate: [validator.isAlpha, 'Your full name can only contain alphabets.']
+        trim: true
     }, 
     contactNumber: {
-        type: Number, 
+        type: String, 
         required: [true, 'You contact number is required.'], 
         trim: true, 
-        validate: {
-            validator: function(value){
-                return value.length < 10
-            }, 
-            message: 'Please provide a valid phone number associated with South African service providers.'
-        }
-        
+        match: /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/
     }, 
     email: {
         type: String, 
@@ -27,17 +19,17 @@ const userSchema = new mongoose.Schema({
         lowercase: true,  
         trim: true, 
         validate: [validator.isEmail, 'Please use a valid email address.']
-        
     }, 
     address: { //come back here as well
         type: String, 
-        required: [true, 'Your address is required']
+        required: [true, 'Your address is required'], 
+        unique: true
     }, 
     bestPractice: {
         type: Boolean, 
-        default: False
+        default: 'false'
     }, 
-    offers: { //come back after some research
+    offers: {                                //come back after some research
         description: String, 
         amount: Number, 
         expires: {
@@ -45,21 +37,22 @@ const userSchema = new mongoose.Schema({
             default: Math.floor(Date.now() + 7 * (3600 * 1000 * 24))
         }, 
         status:{
+            type: String,
             enum: ['Accepted', 'Rejected', 'No response'], 
             default: 'No response'
         }, 
         appointment:{
             type: mongoose.Types.ObjectId, 
             ref: 'Appointment'
-        }, 
+        }}, 
         supportingDocs: [String], //come back to this after research
         verified: {
             type: Boolean, 
-            default: False
+            default: 'false'
         }
-    }
+    
 })
 
-const User =  mongoose.Model('User' , userSchema )
+const User =  mongoose.model('User' , userSchema )
 
 module.exports = User
