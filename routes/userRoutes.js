86c7b0c3003990acf9c45be5e91f56dd88816288
Koketso <koketsomawasha@gmail.com>
2022-12-24 21:41:43 +0200

    user routes defined

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const { route } = require('../app.js')
 const userController = require('../controllers/userController.js')
 
 const router = express.Router()
 
-router.route('/').get(userController.getUsers)
+router.route('/:id').get(userController.getUser)
+
+router.route('/').get(userController.getUsers).post(userController.createUser)
+
 
 
 module.exports = router
