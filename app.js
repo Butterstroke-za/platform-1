3e46bf204055fe135bef2dd5fdce50106792bd3f
Koketso <koketsomawasha@gmail.com>
2022-12-30 02:48:41 +0200

    added sellRoom route and global error placeholder

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const userRouter = require('./routes/userRoutes')
 const appointmentRouter = require('./routes/appointmentRoutes')
 const collectorRouter = require('./routes/collectorRoutes')
+const sellRoomRouter = require('./routes/sellRoomRoutes')
+// const globalError = require('/helpers/globalError.js')
 
 const app = express()
 
@@ -12,6 +14,12 @@ app.use(express.json())
 app.use('/api/v1/users/', userRouter)
 app.use('/api/v1/appointments/', appointmentRouter)
 app.use('/api/v1/collectors/', collectorRouter)
+app.use('/api/v1/sellrooms', sellRoomRouter)
+
+//Global error handler
+app.all('*', (err, req, res , next)=>{
+    console.log('global error handler')
+})
 
 
 module.exports = app
