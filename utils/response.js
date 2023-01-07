b6cf31b8457115ab5bfbac05438646522599acb7
Koketso <koketsomawasha@gmail.com>
2023-01-07 11:44:44 +0200

    Added 201 as potential success statuscode

--- utils/response.js
+++ utils/response.js
@@ -1,5 +1,5 @@
 module.exports = (res, data, statusCode, message) =>{
-    const status = statusCode == 200||204||202? 'success' : 'fail'
+    const status = statusCode == 200||201||202||204? 'success' : 'fail'
 
     return res.status(statusCode).json({
             status,
