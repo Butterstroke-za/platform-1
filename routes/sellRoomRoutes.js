8d087f72544a31b6792db99b844e772b7a2b619f
Koketso Mawasha <koketsomawasha@gmail.com>
2023-01-23 11:10:12 +0200

    Nesting application router in Sell Room

--- routes/sellRoomRoutes.js
+++ routes/sellRoomRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const sellRoomController = require('../controllers/sellRoomControllers')
 const authController = require('../controllers/authController')
+const applicationRouter = require('./applicationRoutes')
 
 const router = express.Router()
 
 router.route('/').post(sellRoomController.createSellRoom).get(sellRoomController.getSellRooms)
 
-router.route('/:id').get(sellRoomController.getSellRoom).patch(sellRoomController.updateSellRoom).delete(sellRoomController.deleteSellRoom).put(authController.protect, sellRoomController.sellRoomApplication)
+router.route('/:id').get(sellRoomController.getSellRoom).patch(sellRoomController.updateSellRoom).delete(sellRoomController.deleteSellRoom)
+
+router.use('/:roomId/applications', applicationRouter)
+
+// put(authController.protect, sellRoomController.sellRoomApplication)
+
 
 
 module.exports = router
