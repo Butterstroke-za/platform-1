30ec5302848bf543bffee729f769ce2988d7a545
Koketso Mawasha <koketsomawasha@gmail.com>
2023-11-05 11:41:25 +0200

    heroku prep

--- server.js
+++ server.js
@@ -7,18 +7,18 @@ const app = require('./app.js')
 const url = process.env.mongoUrl.replace('<PASSWORD>', process.env.mongoPassword).replace('<USERNAME>', process.env.mongoUsername)
 
 mongoose.connect(url, ()=>{
-    console.log('Database connection established '.bold.bgBrightGreen)
+    console.log('Database connection established ')
 }).catch(function(err){
-    console.log('Could not establish database connection '.black.bgBrightRed)
+    console.log('Could not establish database connection ')
 })
 
 
 mongoose.connection.on('error', (err)=>{
-    console.log(`Database connection error: ${err}`.black.bgBrightRed)
+    console.log(`Database connection error: ${err}`)
 })
 
 
 const port = 3000;
 app.listen(port, () =>{
-    console.log(`APP LISTENING ON PORT: ${port}`.yellow.bold)
+    console.log(`APP LISTENING ON PORT: ${port}`)
 })
